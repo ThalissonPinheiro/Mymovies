3d1d69c0bbec91a8e126d80dde83f19209864d4d
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-03-18 22:37:48 +0000

    T004: chore - Exemplo de elemento customizado e importação de um componente em outro

--- angular1/src/app/components/card/card.component.ts
+++ angular1/src/app/components/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
