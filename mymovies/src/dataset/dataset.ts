468bd7effffec7a580295aea818acc2143262273
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-03-21 22:47:31 +0000

    V16-002: feat - Criação e adição de conteúdo no arquivo dataset

--- mymovies/src/dataset/dataset.ts
+++ mymovies/src/dataset/dataset.ts
@@ -1,2 +1,50 @@
-const Movies = {
-}
+const MOVIES_CARD = [
+    {
+        name: 'Kung Fu Panda 4',
+        sinopse: 'Po está prestes a se tornar o novo líder espiritual do Vale da Paz, mas antes que possa fazer isso, ele deve encontrar um sucessor para se tornar o novo Dragão Guerreiro.  Ele parece encontrar uma em Zhen, uma raposa com muitas habilidades promissoras, mas que não gosta muito da ideia de Po treiná-la.',
+        poster: '/aNK6MA5EApIo0UJE7ZWSYcZBJKy.jpg',
+      },
+      {
+        name: 'Godzilla e Kong: O Novo Império',
+        sinopse: 'O poderoso Kong e o temível Godzilla se unem contra uma colossal ameaça mortal escondida no mundo dos humanos, que ameaça a existência de sua espécie e da nossa. Mergulhando profundamente nos mistérios da Ilha da Caveira e nas origens da Terra Oca, o filme irá explorar a antiga batalha de Titãs que ajudou a forjar esses seres extraordinários e os ligou à humanidade para sempre.',
+        poster: '/kO6K9zEsKhNyqcrdGTSqAI6jrie.jpg',
+      },
+      {
+        name: 'The Marvels',
+        sinopse: 'Quando suas obrigações a levam a uma fenda espacial anômala ligada a um revolucionário Kree, os poderes de Carol Danvers, a Capitã Marvel, se entrelaçam aos de Kamala Khan, a Ms. Marvel, e aos da sobrinha de Carol, astronauta da S.A.B.E.R., Capitã Monica Rambeau. Juntas, esse trio improvável deve ser unir para salvar o universo como \"As Marvels\".',
+        poster: '/wpp5KIwjtrKoWj5H0TSsJdqsDx7.jpg',
+      },
+      {
+        name: 'Aquaman 2: O Reino Perdido',
+        sinopse: 'Agora mais temível do que nunca, Arraia Negra detém o poder do mítico Tridente Preto e sua ancestral força malévola. Para derrotá-lo, Aquaman recorrerá a seu irmão Orm, o ex-Rei da Atlântida, ainda na prisão, para forjar uma aliança improvável. Juntos, eles vão ter que deixar de lado suas diferenças para proteger seu reino, salvar a família de Aquaman e o mundo da irreversível destruição.',
+        poster: '/ysHxydAZzfhc4kV5ae8IBbVG5gk.jpg',
+      },
+      {
+        name: 'Trolls 3 - Juntos Novamente',
+        sinopse: 'Depois de dois filmes de amizade verdadeira e paquera implacável, Poppy e Tronco agora são oficialmente, finalmente, um casal! À medida que se aproximam, Poppy descobre que Tronco tem um passado secreto.',
+        poster: '/pUq0VtmlyAm9pFvoKWPBieEOcU8.jpg',
+      },
+      {
+        name: 'Elementos',
+        sinopse: 'Em uma cidade onde moradores do fogo, da água, da terra e do ar vivem juntos, uma jovem impetuosa e um homem tranquilo estão prestes a descobrir algo elementar: o quanto realmente têm em comum.',
+        poster: '/tsfaRkZfUl5Ve9GMV8oY0UuLH52.jpg',
+      },
+      {
+        name: 'Five Nights at Freddys - O Pesadelo Sem Fim',
+        sinopse: 'Enquanto passa sua primeira noite no trabalho, um problemático guarda de segurança da Pizzaria Freddy Fazbear logo percebe que não será uma tarefa fácil sobreviver ao seu primeiro turno.',
+        poster: '/1yUbmAiw2cUSpyXNIaiST7JzCtG.jpg',
+      },
+      {
+        name: 'Leo',
+        sinopse: 'Uma comédia musical que narra o último ano do Ensino Fundamental pelos olhos do lagarto Leo, o bichinho de estimação da sala.',
+        poster: '/pD6sL4vntUOXHmuvJPPZAgvyfd9.jpg',
+      },
+      {
+        name: 'Operação Big Hero',
+        sinopse: 'O grande robô inflável está sempre a postos para cuidar de Hiro Hamada. Quando algo devastador assola a cidade, o menino prodígio, seus amigos e o robô formam um grupo de heróis para combater o mal.',
+        poster: '/5EodFesjReSDlX1c0I5ANZijBFy.jpg',
+      },
+  
+]
+
+export default MOVIES_CARD;
