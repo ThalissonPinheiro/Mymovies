b361956cae447aa5fef07db2cb51e83766b1c029
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-04-02 22:23:51 +0000

    V16-003: feat - Importação do componente Card e do Dataset no componente CardsList

--- mymovies/src/app/components/cards-list/cards-list.component.ts
+++ mymovies/src/app/components/cards-list/cards-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { MOVIES_CARDS } from 'src/dataset/dataset';
 
 @Component({
   selector: 'app-cards-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CardComponent
+  ],
   templateUrl: './cards-list.component.html',
   styleUrls: ['./cards-list.component.css']
 })
 export class CardsListComponent {
-
+  MOVIES = MOVIES_CARDS.results;
 }
