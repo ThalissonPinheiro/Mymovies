2ee87bcb38485b1df3ea8e651f994ff99128f7dc
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-04-05 06:51:38 +0000

    V16-004: feat - Implementação da busca dos detalhes dos filmes

--- mymovies/src/app/pages/details/details.component.ts
+++ mymovies/src/app/pages/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MOVIES_DETAILS } from 'src/dataset/dataset';
 
 @Component({
   selector: 'app-details',
@@ -8,11 +9,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent {
   movieName: string = '';
+  movies = MOVIES_DETAILS.results;
+  movie: any;
+
+
 
   constructor(
     readonly dadosRota: ActivatedRoute
   ) {
     this.movieName = this.dadosRota.snapshot.params['movieName'];
+    this.loadMovieDetails();
+  }
+
+  loadMovieDetails() {
+    this.movie = this.movies.find(filme => filme.title === this.movieName);
+
+    console.log(this.movie);
+
   }
 
 }
