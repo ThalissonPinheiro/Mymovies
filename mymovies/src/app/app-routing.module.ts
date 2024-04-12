e307b1eb687c4bf6ea590c2c148e4f28479a0990
Thalisson Pinheiro <thalisson.pw3@gmail.com>
2024-04-12 04:39:42 +0000

    V16-005: chore - Criado a rota para as pages e declarado as novas pages no app.modules

--- mymovies/src/app/app-routing.module.ts
+++ mymovies/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { TerirComponent } from './pages/terir/terir.component';
+import { BesteirolComponent } from './pages/besteirol/besteirol.component';
+import { RomanticaComponent } from './pages/romantica/romantica.component';
+import { AnimacaoComponent } from './pages/animacao/animacao.component';
 
 const routes: Routes = [
   {
@@ -14,6 +18,26 @@ const routes: Routes = [
     path: 'home',
     title: 'MyMovies - Home',
     component: HomeComponent
+  },
+  {
+    path: 'terir',
+    title: 'MyMovies - Terir',
+    component: TerirComponent
+  },
+  {
+    path: 'romantica',
+    title: 'MyMovies - Romantica',
+    component: RomanticaComponent
+  },
+  {
+    path: 'animacao',
+    title: 'MyMovies - Animação',
+    component: AnimacaoComponent
+  },
+  {
+    path: 'besteirol',
+    title: 'MyMovies - Besteirol',
+    component: BesteirolComponent
   },
   {
     path: 'details/:movieName',
