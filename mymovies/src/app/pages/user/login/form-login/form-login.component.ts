219d8dd69ae776a0fd820cfc5bc4d845d4a2cabc
Thalisson Pinheiro <thalissontec2023@gmail.com>
2024-05-23 22:26:47 +0000

    V16-011: chore - Criação básica do componente form-login

---
+++ mymovies/src/app/pages/user/login/form-login/form-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-login.component.html',
+  styleUrls: ['./form-login.component.css']
+})
+export class FormLoginComponent {
+
+}
